2cca5eee762bf28685e256cc540559bc7a806d5b
GauthierDefrance <defrance.gauthier@gmail.com>
2024-09-15 22:08:43 +0200

    added desc to Control

--- Code/Usefull Lib/control.py
+++ Code/Usefull Lib/control.py
@@ -3,6 +3,9 @@ from screen import *
 from interface import *
 
 def Control(MyScreen,joystick,interface):
+    """A simple function, take an interface List, a joystick object and a Screen.
+    If the joystick goes up,down,right or left. It will automatically change 
+    the currently selected box."""
     x,y = joystick.getDirections()
     if y=="Up":
         interface.nextY(-1)
